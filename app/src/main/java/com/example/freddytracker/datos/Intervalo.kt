7caa7ba31c2812ebc426681ef1c300a33dc30e6d
jeshuayam <jeshuayam3024@gmail.com>
2026-03-22 13:49:22 -0600

    Estado añadido a la clase de intervalo

--- app/src/main/java/com/example/freddytracker/datos/Intervalo.kt
+++ app/src/main/java/com/example/freddytracker/datos/Intervalo.kt
@@ -4,10 +4,17 @@ data class Intervalo(
     val id: Int,
     var nombre: String,
     var duracion: Long,
-    var tipo: TipoIntervalo
+    var tipo: TipoIntervalo,
+    var estado: EstadoIntervalo
 )
 
 enum class TipoIntervalo {
     ACTIVIDAD,
     DESCANSO
+}
+
+enum class EstadoIntervalo {
+    PENDIENTE,
+    EN_PROGRESO,
+    FINALIZADO
 }
